c4944531f67fc0facdf6684d74ebdb0e0a9704d0
ravi@localhost.localdomain <ravi@localhost.localdomain>
2008-12-17 17:16:01 -0600

    mostly works

--- xpipe.py
+++ xpipe.py
@@ -6,16 +6,27 @@ import re
 from select import select
 from subprocess import Popen, PIPE
 
+import time
+
 # XPipe prototype - might rewrite in C later to reduce memory overhead and dependencies
 # Making this a binary will also allow us to use #! in pipe files, and use them as programs themselves, which would be neat
 
+# TODO: special STDIN and STDOUT commands, so we can handle them
+
 class GraphNodeStream:
-	def __init__(self, node, stream):	
+	def __init__(self, node, stream, name):	
 		self.node = node
 		self.stream = stream
+		self.name = name
 	
 	def fileno(self):
 		return self.stream.fileno()
+	
+	def is_live(self):
+		return self.stream is not None and not self.stream.closed
+	
+	def __repr__(self):
+		return self.node.__repr__() + "." + self.name
 
 class GraphNode:
 	def __init__(self, name, command):
@@ -27,17 +38,31 @@ class GraphNode:
 		self.outputs = []
 	
 	def execute(self):
-		self.process = Popen(self.command, shell=True, stdin=PIPE, stdout=PIPE, stderr=None)
-		self.stdin = GraphNodeStream(self, self.process.stdin)
-		self.stdout = GraphNodeStream(self, self.process.stdout)
+		if self.command is not None:
+			self.process = Popen(self.command, shell=True, stdin=PIPE, stdout=PIPE, stderr=None)
+			self.stdin = GraphNodeStream(self, self.process.stdin, 'stdin')
+			self.stdout = GraphNodeStream(self, self.process.stdout, 'stdout')
+	
+	def is_live(self):
+		if self.process is not None:
+			self.process.poll()
+		return self.command is None or (self.process is not None and self.process.returncode is None)
+	
+	def is_readable(self):
+		return self.stdout.is_live()
+	
+	def is_writable(self):
+		return self.stdin.is_live()
 	
 	def __repr__(self):
-		return "(" + self.name + ": " + ",".join([ x.name for x in self.outputs ]) + ")"
+		if self.command is None:
+			return "(" + self.name + ")"
+		return "(" + self.name + ": " + self.command + ")"
 	
 
 ### Read input files
 
-# map of command names to commands
+# map of command names to commands (later, becomes a map of command names to GraphNode instances)
 cmds = {}
 
 # list of (name, name) tuples
@@ -82,6 +107,17 @@ for line in f:
 for cmd in cmds:
 	cmds[cmd] = GraphNode(cmd, cmds[cmd])
 
+# create special stdin and stdout commands
+fake_stdout = GraphNode('STDOUT', None)
+fake_stdout.stdin  = GraphNodeStream(fake_stdout, sys.stdout, 'stdin')
+fake_stdout.stdout = GraphNodeStream(fake_stdout, None, 'stdout')
+cmds['STDOUT'] = fake_stdout
+
+fake_stdin = GraphNode('STDIN', None)
+fake_stdin.stdin  = GraphNodeStream(fake_stdin, None, 'stdin')
+fake_stdin.stdout = GraphNodeStream(fake_stdin, sys.stdin, 'stdout')
+cmds['STDIN'] = fake_stdin
+
 # second pass - fill in outputs with GraphNode objects
 for edge in graph:
 	for cmd in edge[0], edge[1]:
@@ -90,15 +126,53 @@ for edge in graph:
 			sys.exit(1)
 	cmds[edge[0]].outputs.append(cmds[edge[1]])
 
-print cmds
+#print cmds
 
 ### Process pipeline data in a loop
-rm = """
-while len(cmds) < 0:
-	r = [ cmds[x][1] for x in cmds] # read from stdout streams
-	w = [ cmds[x][0] for x in cmds] # write to stdin streams
-	x = r + w # dunno what should go in here :(
+
+# execute all commands
+for cmd in cmds:
+	cmds[cmd].execute()
+
+while len(cmds) > 0:
+	r = [ cmds[x].stdout for x in cmds if cmds[x].is_readable() ] # read from stdout streams
+	w = [ cmds[x].stdin for x in cmds if cmds[x].is_writable() ]  # write to stdin streams
+	x = r + w                                                     # dunno what should go in here :(
 	
 	r, w, x = select(r, w, x)
 	
-"""
+	print "SELECTED: ",
+	print r,w,x
+	
+	for readable in r:
+		print "trying " + str(readable)
+		can_read = True
+		for out in readable.node.outputs:
+			print "   is " + str(out) + " writable?"
+			if out.stdin not in w:
+				print str(out) + " is not writable, skipping " + str(readable)
+				can_read = False
+		
+		if can_read:
+			print "   can read!"
+			data = readable.stream.read(65536)
+			print "   read data from " + str(readable.node) + ": " + data
+			for out in readable.node.outputs:
+				out.stdin.stream.write(data)
+				print "   wrote data to " + str(out)
+	
+	dead = []
+	for cmd in cmds:
+		if not cmds[cmd].is_live():
+			print "!!! %s is no longer live!" % cmds[cmd]
+			dead.append(cmd)
+		for out in cmds[cmd].outputs:
+			if not out.is_live():
+				cmds[cmd].outputs.remove(out)
+	for process in dead:
+		for out in cmds[process].outputs:
+			out.stdin.stream.close()
+		del cmds[process]
+	
+	time.sleep(1)
+	
